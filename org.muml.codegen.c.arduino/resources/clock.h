7bb52b7f2558a8dd7ff510ee383e88efbe020508
Uwe Pohlmann <upohl>
2016-11-10 13:14:31 +0000

    fixed the standard clock implementation

--- org.muml.codegen.c.arduino/resources/clock.h
+++ org.muml.codegen.c.arduino/resources/clock.h
@@ -1,30 +1,38 @@
-/*********************************************************************************
- * Copyright (c) 2012 Solunar GmbH and others.
- * All rights reserved.
- *
- * Contributors:
- *     Solunar GmbH and others - initial API and implementation
- *******************************************************************************/
 /**
  * @file
- * @author Solunar
- * @brief This file specifies how Clocks are read for a specific Target Platform
- * @details The function to read and set Clocks are defined as Preprocessor MACROS
- * @warning This needs to be adapted for specific Target Platforms
+ * @author Fraunhofer IPT-EM
+ * @brief This file specifies the MessageBuffer, which are contained at the Components
+ * @details  The MessageBuffer are used at the Components to store  MiddlewareMessage%s
  */
 #ifndef CLOCK_H_
 #define CLOCK_H_
-#include "OS.h"
+//#include "../Middleware/OS.h"
 
 
-
-
-#ifdef ARDUINO
-unsigned long millis(void);
+#ifdef NXTOSEK
+#include "ecrobot_interface.h"
+#include <time.h>
 typedef long Clock;
-#define Clock_getTime(aClock) (  (millis() - (aClock)) )
+#define Clock_getTime(aClock) (  (systick_get_ms() - (aClock)) )
+#define Clock_reset(aClock) ((aClock) = systick_get_ms())
+#endif /* NXTOSEK */
 
-#define Clock_reset(aClock) ((aClock) = millis())
+#ifdef C99
+#include <time.h>
+#include <stdtypes.h>
+typedef uint64_t Clock;
+static inline Clock Clock_getTime2(Clock aClock) {
+	struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+	return spec.tv_sec * 1000 + (spec.tv_nsec / 1.0e6) - aClock;
+}
+static inline void Clock_reset2(Clock* clock) { *clock = Clock_getTime2(0); }
+
+
+#define Clock_getTime(aClock) Clock_getTime2(aClock)
+#define Clock_reset(aClock) Clock_reset2(&aClock)
+
+
 #endif /* C99 */
 
 
--- org.muml.codegen.c/resources/lib/clock.h
+++ org.muml.codegen.c/resources/lib/clock.h
@@ -6,7 +6,7 @@
  */
 #ifndef CLOCK_H_
 #define CLOCK_H_
-#include "../Middleware/OS.h"
+//#include "../Middleware/OS.h"
 
 
 #ifdef NXTOSEK
@@ -14,19 +14,26 @@
 #include <time.h>
 typedef long Clock;
 #define Clock_getTime(aClock) (  (systick_get_ms() - (aClock)) )
-
 #define Clock_reset(aClock) ((aClock) = systick_get_ms())
 #endif /* NXTOSEK */
 
 #ifdef C99
 #include <time.h>
-typedef long Clock;
-#define Clock_getTime(aClock) ( (CLOCKS_PER_SEC == 1000) ? (clock() - (aClock)) : ((clock() - (aClock)) * 1000 / CLOCKS_PER_SEC) )
+#include <stdtypes.h>
+typedef uint64_t Clock;
+static inline Clock Clock_getTime2(Clock aClock) {
+	struct timespec spec;
+    clock_gettime(CLOCK_REALTIME, &spec);
+	return spec.tv_sec * 1000 + (spec.tv_nsec / 1.0e6) - aClock;
+}
+static inline void Clock_reset2(Clock* clock) { *clock = Clock_getTime2(0); }
 
-#define Clock_reset(aClock) ((aClock) = clock())
+
+#define Clock_getTime(aClock) Clock_getTime2(aClock)
+#define Clock_reset(aClock) Clock_reset2(&aClock)
+
+
 #endif /* C99 */
-
-
 
 
 #endif /* CLOCK_H_ */
